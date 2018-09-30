db47d534b888dfe7c87a39909168220f9be76bfc
Victor Hugo Olvera Cruz <victor olvera@tiempodev.local>
2018-09-30 15:33:21 -0500

    Spring Boot REST Jackson Maven MySQL JPA Example

---
+++ src/main/java/com/studyrest/studyRest/StudyRestApplication.java
@@ -0,0 +1,12 @@
+package com.studyrest.studyRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyRestApplication.class, args);
+	}
+}
